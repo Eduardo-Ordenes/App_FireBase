ff497a43f2c4b225277cabc2918f127893f93099
Eduardo <eduardoordenes421@gmail.com>
2022-12-03 09:03:20 -0300

    V1 AppFireBase

--- app/src/main/java/com/example/app_firebase/MainActivity.java
+++ app/src/main/java/com/example/app_firebase/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.app_firebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnbuscar;
     private Button btnmodificar;
     private Button btneliminar;
+    private ListView lvDatos;
 
 
     @Override
@@ -43,14 +53,16 @@ public class MainActivity extends AppCompatActivity {
         txtnombre = (EditText) findViewById(R.id.txtNombre);
         txttelefono = (EditText) findViewById(R.id.txtTelefono);
         txtid = (EditText) findViewById(R.id.txtId);
+        lvDatos = (ListView) findViewById(R.id.lvDatos);
 
 
     }
 
 
-    private void botonRegistrar(){
-        btnregistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+    //private void botonRegistrar(){
+        //btnregistrar.setOnClickListener(new View.OnClickListener() {
+            //@Override
             public void onClick(View view) {
                 if(txtid.getText().toString().trim().isEmpty()
                         || txtnombre.getText().toString().trim().isEmpty()
@@ -110,18 +122,271 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
+    //    });
+    //} // Cierra el método botonRegistrar.
+
+
+    //private void botonModificar(){
+    //    btnmodificar.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick2(View view) {
+
+                if(txtid.getText().toString().trim().isEmpty()
+                        || txtnombre.getText().toString().trim().isEmpty()
+                        || txttelefono.getText().toString().trim().isEmpty()
+                        || txtcorreo.getText().toString().trim().isEmpty()
+                )
+                {
+                    ocultarTeclado();
+                    Toast.makeText(MainActivity.this, "Complete los campos faltantes para actualizar!!", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    int id = Integer.parseInt(txtid.getText().toString());
+                    String nombre = txtnombre.getText().toString();
+                    String telefono = txttelefono.getText().toString();
+                    String correo = txtcorreo.getText().toString();
+
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference dbref = db.getReference(Agenda.class.getSimpleName());
+
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            boolean res2 = false;
+                            for(DataSnapshot x : snapshot.getChildren()){
+                                if(x.child("nombre").getValue().toString().equalsIgnoreCase(nombre)){
+                                    res2 = true;
+                                    ocultarTeclado();
+                                    Toast.makeText(MainActivity.this, "El nombre ("+nombre+") ya existe.\nimposible modificar!!", Toast.LENGTH_SHORT).show();
+                                    break;
+                                }
+                            }
+
+                            if(res2 == false){
+                                String aux = Integer.toString(id);
+                                boolean res = false;
+                                for(DataSnapshot x : snapshot.getChildren()){
+                                    if(x.child("id").getValue().toString().equalsIgnoreCase(aux)){
+                                        res = true;
+                                        ocultarTeclado();
+                                        x.getRef().child("nombre").setValue(nombre);
+                                        x.getRef().child("telefono").setValue(telefono);
+                                        x.getRef().child("correo").setValue(correo);
+                                        limpiar();
+                                        listarContactos();
+                                        break;
+                                    }
+                                }
+
+                                if(res == false){
+                                    ocultarTeclado();
+                                    Toast.makeText(MainActivity.this, "ID ("+aux+") no encontrado.\nimposible modificar!!!!", Toast.LENGTH_SHORT).show();
+                                    txtid.setText("");
+                                    txtnombre.setText("");
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                } // Cierra el if/else inicial.
+
+            }
+    //    });
+    //} // Cierra el método botonModificar.
+
+
+
+    public void listarContactos(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference dbref = db.getReference(Agenda.class.getSimpleName());
+
+        ArrayList<Agenda> listaagenda = new ArrayList<Agenda>();
+        ArrayAdapter<Agenda> adapter = new ArrayAdapter <Agenda> (MainActivity.this, android.R.layout.simple_list_item_1, listaagenda);
+        lvDatos.setAdapter(adapter);
+
+
+        dbref.addChildEventListener(new ChildEventListener() {
+            @Override
+            //ocurre cuando se agregar un nuevo registro
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Agenda agenda = snapshot.getValue(Agenda.class);
+                listaagenda.add(agenda);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            // ocurre cuando se modifica o elimina un registro
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
-    } // Cierra el método botonRegistrar.
+
+
+        lvDatos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Agenda agenda = listaagenda.get(i);
+                AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
+                a.setCancelable(true);
+                a.setTitle("Contacto Seleccionado");
+                String msg = "ID : " + agenda.getId() +"\n\n";
+                msg += "NOMBRE : " + agenda.getNombre();
+                a.setMessage(msg);
+                a.show();
+            }
+        });
+
+    } // Cierra el método
 
 
 
-    private void ocultarTeclado(){
+    //private void botonBuscar(){
+        //btnbuscar.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick3(View view) {
+                if(txtid.getText().toString().trim().isEmpty()){
+                    ocultarTeclado();
+                    Toast.makeText(MainActivity.this, "Digite el ID del contacto a buscar!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    int id = Integer.parseInt(txtid.getText().toString());
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference dbref = db.getReference(Agenda.class.getSimpleName());
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String aux = Integer.toString(id);
+                            boolean res = false;
+                            for(DataSnapshot x : snapshot.getChildren()){
+                                if(aux.equalsIgnoreCase(x.child("id").getValue().toString())){
+                                    res = true;
+                                    ocultarTeclado();
+                                    txtnombre.setText(x.child("nombre").getValue().toString());
+                                    txttelefono.setText(x.child("telefono").getValue().toString());
+                                    txtcorreo.setText(x.child("correo").getValue().toString());
+                                    break;
+                                }
+                            }
+                            if(res == false){
+                                ocultarTeclado();
+                                Toast.makeText(MainActivity.this, "ID ("+aux+") No encontrado!!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                } // Cierra el if/else inicial.
+
+            }
+    //    });
+    //} // Cierra el método botonBuscar.
+
+    //private void botonEliminar(){
+    // btneliminar.setOnClickListener(new View.OnClickListener() {
+    //    @Override
+        public void onClick4(View view) {
+            if(txtid.getText().toString().trim().isEmpty()){
+                ocultarTeclado();
+                Toast.makeText(MainActivity.this, "Digite el ID del contacto a eliminar!!", Toast.LENGTH_SHORT).show();
+            }else{
+                int id = Integer.parseInt(txtid.getText().toString());
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference dbref = db.getReference(Agenda.class.getSimpleName());
+                dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        String aux = Integer.toString(id);
+                        final boolean[] res = {false};
+                        for(DataSnapshot x : snapshot.getChildren()){
+                            if(aux.equalsIgnoreCase(x.child("id").getValue().toString())){
+                                AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
+                                a.setCancelable(false);
+                                a.setTitle("Pregunta");
+                                a.setMessage("¿Está seguro(a) de querer eliminar el registro?");
+                                a.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                });
+
+                                a.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        res[0] = true;
+                                        ocultarTeclado();
+                                        x.getRef().removeValue();
+                                        listarContactos();
+                                    }
+                                });
+                                a.show();
+                                break;
+                            }
+                        }
+
+                        if(res[0] == false){
+                            ocultarTeclado();
+                            Toast.makeText(MainActivity.this, "ID ("+aux+") No encontrado.\nimposible eliminar!!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            } // Cierra el if/else inicial.
+
+        }
+//    });
+//} // Cierra el método botonEliminar.
+
+
+
+
+
+
+
+
+
+
+
+
+    public void ocultarTeclado() {
 
         View view = this.getCurrentFocus();
-        if(view != null){
+        if (view != null) {
 
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
 
         }
 
@@ -129,14 +394,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void limpiar(){
+    public void limpiar() {
 
         txtid.setText("");
         txtnombre.setText("");
         txtcorreo.setText("");
-        txtcorreo.setText("");
-
-
+        txttelefono.setText("");
 
     }
 
